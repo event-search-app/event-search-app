c3d67f16776d179f126d8dac5870b1d41b86e52c
event-search-app <ranran1637@gmail.com>
2025-03-20 11:36:43 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => res.status(200).send('Relay server ready✨'));
 app.post('/', async (req, res) => {
   console.log('LINEからPOSTリクエストを受信✨');
   try {
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbwwtfSKSO9zNfMZc_eg-Omdjv_ZFej_OgFqn2Ngq2lpKRlY6qKDx71Gd2akfUSx_aQr7w/exec';
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbxSCyiyOQ22Bj2tgjZv9X6MS4mIthl_wQ9I-VAI_vFM__gFRKajlduCj4woyzymUaWINg/exec';
     const result = await axios.post(gasUrl, req.body);
     console.log('GASへのリクエスト成功:', result.data);
     res.status(200).send(result.data);
